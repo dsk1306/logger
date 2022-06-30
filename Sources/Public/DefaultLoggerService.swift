7d24b3bee569807e7e1d420afec8e5ad5c8598ce
Dmitry Kuznetsov <dsk1306@icloud.com>
2022-06-30 22:06:06 +0200

    Added DefaultLoggerService

---
+++ Sources/Public/DefaultLoggerService.swift
@@ -0,0 +1,116 @@
+import Bugsnag
+import Foundation
+
+final class DefaultLoggerService {
+
+  // MARK: - Properties
+
+  var onOptOut: OnOptOutHandler?
+
+  // MARK: - Initialization
+
+  init(onOptOut: OnOptOutHandler?) {
+    self.onOptOut = onOptOut
+  }
+
+}
+
+// MARK: - LoggerService
+
+extension DefaultLoggerService: LoggerService {
+
+  func log(error: String) {
+    let error = StringError(errorString: error)
+    log(error: error)
+  }
+
+  func log(error: Error) {
+    if let error = error as? LoggableError {
+      guard error.shouldReport else { return }
+      Bugsnag.notifyError(error) { report in
+        report.writeErrorClass(from: error)
+        report.writeParameters(from: error)
+        report.writeContext(from: error)
+        report.writeErrorMessage(from: error)
+        return true
+      }
+    } else {
+      Bugsnag.notifyError(error)
+    }
+  }
+
+  func log(deinitOf object: Any) {
+    #if DEBUG
+    let typeOfObject = type(of: object)
+    let describing = String(describing: typeOfObject)
+    print(describing)
+    #endif
+  }
+
+  func log(items: Any...) {
+    #if DEBUG
+    print(items)
+    #endif
+  }
+
+}
+
+// MARK: - Constants
+
+private extension DefaultLoggerService {
+
+  enum Constant {
+
+    static let sectionName = "UserInfo"
+
+  }
+
+}
+
+// MARK: - BugsnagEvent Extension
+
+private extension BugsnagEvent {
+
+  func writeParameters(from error: LoggableError) {
+    guard let parameters = error.analyticParameters else { return }
+    for (key, value) in parameters {
+      addMetadata(
+        value,
+        key: key,
+        section: DefaultLoggerService.Constant.sectionName
+      )
+    }
+  }
+
+  func writeContext(from error: LoggableError) {
+    guard let context = error.analyticContext else { return }
+    self.context = context
+  }
+
+  func writeErrorClass(from error: LoggableError) {
+    errors.forEach {
+      $0.errorClass = "\(type(of: error))"
+    }
+  }
+
+  func writeErrorMessage(from error: LoggableError) {
+    errors.forEach {
+      $0.errorMessage = error.analyticMessage
+    }
+  }
+
+}
+
+// MARK: - String Error
+
+extension DefaultLoggerService {
+
+  struct StringError: LoggableError {
+
+    let errorString: String?
+
+    var analyticMessage: AnalyticMessage { errorString }
+
+  }
+
+}
