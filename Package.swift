f0908c4d1ebffc3d6ad09087ebe24b21adad43a2
Dmitry Kuznetsov <dsk1306@icloud.com>
2022-06-30 22:25:50 +0200

    Updated Package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,11 @@ import PackageDescription
 let package = Package(
   name: "Logger",
   platforms: [
-    .iOS(.v15)
+    .iOS(.v15),
+    .macOS(.v12),
+    .macCatalyst(.v15),
+    .tvOS(.v15),
+    .watchOS(.v8)
   ],
   products: [
     .library(
